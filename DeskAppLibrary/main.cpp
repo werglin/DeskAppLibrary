75fbd3ce67fe3b365e549f36baad0c9aed46c7aa
Salih <salih.esme@hotmail.com>
2022-11-15 14:26:16 +0300

    Program State => Basic Window Don need main. go wWinMain // Program State => Basic Windows Learned

--- DeskAppLibrary/main.cpp
+++ DeskAppLibrary/main.cpp
@@ -2,7 +2,25 @@
 
 #include"WinMain.cpp"
 
-int main()
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
-	return wWinMain(NULL, NULL, NULL, SW_SHOW);
+    MainWindow win;
+
+    if (!win.Create(_T("Learn to Program Windows"), WS_OVERLAPPEDWINDOW))
+    {
+        return 0;
+    }
+
+    ShowWindow(win.Window(), nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg); 
+        DispatchMessage(&msg);
+    }
+
+    return 0;
 }
